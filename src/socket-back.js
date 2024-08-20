3416a5f680af5a8ae1e38496f7cf9e3d3587fdfc
Gabriel Medrado <medradoogabriel@gmail.com>
2024-08-20 10:50:37 -0300

    adjust

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,10 +1,29 @@
 import chalk from 'chalk';
 import io from './server.js';
 
+const documentos = [
+  {
+    nome: 'JavaScript',
+    texto: 'texto de javascript...',
+  },
+  {
+    nome: 'Node',
+    texto: 'texto de node...',
+  },
+  {
+    nome: 'Socket.io',
+    texto: 'texto de socket.io...',
+  },
+];
+
 io.on('connection', (socket) => {
   console.log(chalk.greenBright('Um cliente se conectou ! ID: ', socket.id));
 
   socket.on('selecionar_documento', (nomeDocumento) => {
+    const documento = encontrarDocumento(nomeDocumento);
+
+    console.log(documento);
+
     socket.join(nomeDocumento);
   });
 
@@ -19,3 +38,11 @@ io.on('connection', (socket) => {
     );
   });
 });
+
+function encontrarDocumento(nome) {
+  const documento = documentos.find((documento) => {
+    return documento.nome === nome;
+  });
+
+  return documento;
+}
